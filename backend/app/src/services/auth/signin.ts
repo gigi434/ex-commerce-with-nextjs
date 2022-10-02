fe7127c8a591d54f2d59b0fe6b71187daac971e4
wagatsuma.yuichi <firefly.of.april@gmail.com>
2022-10-03 05:12:46 +0900

    [add] #1 クライアントサイドのAPI関数を作成した

---
+++ backend/app/src/services/auth/signin.ts
@@ -0,0 +1,32 @@
+import { ApiContext, User } from 'types'
+import { fetcher } from '../../utils'
+
+export type SigninParams = {
+    username: string,
+    password: string
+}
+
+/*
+認証API（サインイン）
+@param contxt APIコンテキスト　静的サイト生成時のリクエストとクライアントサイドのリクエストを分ける
+@param params パラメータ
+@returns ログインユーザー
+*/
+
+const signin = async (
+    context: ApiContext,
+    params: SigninParams,
+): Promise<User> => {
+    return await fetcher(
+        `${context.apiRootUrl.replace(/\/$/g,'')}/auth/signin`,
+        {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        }
+    )
+}
+
+export default signin
