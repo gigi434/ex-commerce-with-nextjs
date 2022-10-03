44b20c4caccddb96f36a7df3f0f7f2e15c872d95
wagatsuma.yuichi <firefly.of.april@gmail.com>
2022-10-04 08:39:18 +0900

    [add] #3 レスポンシブデザインに対応したコンポーネント

--- frontend/app/src/themes/index.ts
+++ frontend/app/src/themes/index.ts
@@ -10,4 +10,6 @@ export const theme = {
   letterSpacings,
   lineHeights,
   colors,
+  // const assertionとは全ての値を読み取り専用及び変更不可にするアサーションです。
+  //  記載方法はオブジェクトの末尾にas constを記述すことで、プロパティをreadonlyで指定した場合と同等の扱いにすることができます。
 } as const
